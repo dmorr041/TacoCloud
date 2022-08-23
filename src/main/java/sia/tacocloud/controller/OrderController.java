2e04f85bae9e2efec66e3c0cf562d8460e9c1a5b
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-08-23 12:25:26 -0500

    Fixed the process order flow - fixed TacoOrder to have matching fields as in the view, added IngredientByIdConverter

--- src/main/java/sia/tacocloud/controller/OrderController.java
+++ src/main/java/sia/tacocloud/controller/OrderController.java
@@ -3,17 +3,27 @@ package sia.tacocloud.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import sia.tacocloud.entities.TacoOrder;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
-public class OrdersController {
+public class OrderController {
 
     @GetMapping("/current")
-    public String currentOrders() {
+    public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
--- src/main/java/sia/tacocloud/entities/TacoOrder.java
+++ src/main/java/sia/tacocloud/entities/TacoOrder.java
@@ -2,14 +2,21 @@ package sia.tacocloud.entities;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
     private String deliveryName;
-    private Address deliveryAddress;
-    private CreditCard deliveryCreditCard;
-    private List<Taco> tacos;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
         this.tacos.add(taco);
