b561232be875c608f28995f54534f8702a4a5876
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-10-03 16:52:02 -0500

    Added some identifiers and timestamp fields to our Taco and TacoOrder entities

--- src/main/java/sia/tacocloud/entities/Taco.java
+++ src/main/java/sia/tacocloud/entities/Taco.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -16,4 +17,8 @@ public class Taco {
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    private Long id;
+
+    private Date createdAt = new Date();
 }
--- src/main/java/sia/tacocloud/entities/TacoOrder.java
+++ src/main/java/sia/tacocloud/entities/TacoOrder.java
@@ -5,9 +5,9 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -42,6 +42,10 @@ public class TacoOrder {
 
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private Date placedAt;
 
     private List<Taco> tacos = new ArrayList<>();
 
