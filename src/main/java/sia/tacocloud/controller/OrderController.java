9da6017996fd491ff2046e8556fb04145baba087
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-10-24 13:08:47 -0500

    Injected an OrderRepository into the OrderController and integrated it

--- src/main/java/sia/tacocloud/controller/OrderController.java
+++ src/main/java/sia/tacocloud/controller/OrderController.java
@@ -1,5 +1,6 @@
 package sia.tacocloud.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.entities.TacoOrder;
+import sia.tacocloud.repositories.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -16,7 +18,10 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderRepository orderRepo;
 
     @GetMapping("/current")
     public String orderForm() {
@@ -29,7 +34,7 @@ public class OrderController {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
