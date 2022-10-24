4fa05341b59d418ba8d16737262d7d66bc8d27cb
Darien Quinn Morrison <dmorr041@fiu.edu>
2022-10-24 10:36:19 -0500

    Replaced hard coded ingredients with calls to IngredientRepository

--- src/main/java/sia/tacocloud/controller/DesignTacoController.java
+++ src/main/java/sia/tacocloud/controller/DesignTacoController.java
@@ -1,7 +1,8 @@
 package sia.tacocloud.controller;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,36 +16,33 @@ import sia.tacocloud.entities.Ingredient;
 import sia.tacocloud.entities.TacoOrder;
 import sia.tacocloud.entities.Ingredient.Type;
 import sia.tacocloud.entities.Taco;
+import sia.tacocloud.repositories.JdbcIngredientRepository;
+
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+
+    private final JdbcIngredientRepository ingredientRepository;
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-            new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-            new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-            new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-            new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-            new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-            new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-            new Ingredient("CHED", "Cheddar", Type.CHEESE),
-            new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-            new Ingredient("SLSA", "Salsa", Type.SAUCE),
-            new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        Iterable<Ingredient> ingredients = ingredientRepository.findAll();
         Type[] types = Ingredient.Type.values();
-        for (Type type : types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+
+        for(Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType((List<Ingredient>) ingredients, type));
         }
     }
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order() {
         return new TacoOrder();
     }
+
 
     @ModelAttribute(name = "taco")
     public Taco taco() {
